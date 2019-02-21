60d1f5e4803e17bc0e4d2c2170f342137efb6cf3
Anton Mattsson <anton.matinpoika@gmail.com>
2019-02-21 17:45:38 +0200

    Bugfix to missingness flagging

--- R/quality_metrics.R
+++ R/quality_metrics.R
@@ -1,11 +1,5 @@
 
-#' Compute quality metrics for each feature
-#' 
-#' @param object A MetaboSet object
-#' 
-#' @details LOL
-#' 
-#' @return A MetaboSet object, with the quality metrics added to feature data
+
 setGeneric("assess_quality", signature = "object",
            function(object) standardGeneric("assess_quality"))
 
@@ -22,14 +16,10 @@ setMethod("assess_quality", c(object = "MetaboSet"),
                          RSD_r = finite_mad(qc_data[i, ]) / abs(finite_median(qc_data[i, ])),
                          D_ratio = finite_sd(qc_data[i, ]) / finite_sd(sample_data[i, ]),
                          D_ratio_r = finite_mad(qc_data[i, ]) / finite_mad(sample_data[i, ]),
-                         Detection_rate = 1 - prop_na(qc_data[i, ]),
                          stringsAsFactors = FALSE)
             }
 
-            fData(object) <- dplyr::left_join(fData(object),
-                                                       quality_metrics,
-                                                       by = "Feature_ID")
-            rownames(fData(object)) <- fData(object)$Feature_ID
+            object <- join_fdata(object, quality_metrics)
 
             object
           })
@@ -42,7 +32,7 @@ setGeneric("quality", signature = "object",
 setMethod("quality", c(object = "MetaboSet"),
           function(object) {
             fData(object)[c("Feature_ID", "RSD", "RSD_r", "D_ratio",
-                                     "D_ratio_r", "Detection_rate")]
+                                     "D_ratio_r")]
           })
 
 
@@ -69,16 +59,50 @@ setMethod("flag_quality", c(object = "MetaboSet"),
           })
 
 
-setGeneric("flag_missigness", signature = "object",
-           function(object, qc_limit, group_limit, group) standardGeneric("flag_missigness"))
+setGeneric("flag_detection", signature = "object",
+           function(object, qc_limit = 0.7, group_limit = 0.8, group = group_col(object)) standardGeneric("flag_detection"))
 
-#' @importFrom Biobase fData "fData<-"
+#' @importFrom Biobase fData "fData<-" featureNames
 #' @importFrom magrittr "%>%"
-setMethod("flag_missigness", c(object = "MetaboSet"),
-          function(object, qc_limit = 0.7, group_limit = 0.8, group = group(object)) {
+setMethod("flag_detection", c(object = "MetaboSet"),
+          function(object, qc_limit, group_limit, group) {
+
+            found_qc <- Biobase::esApply(object[, object$QC == "QC"], 1, prop_found)
+            bad_qc <- names(which(found_qc < qc_limit))
+
+            found_qc_df <- data.frame(Feature_ID = names(found_qc),
+                                      Detection_rate_QC = found_qc,
+                                      stringsAsFactors = FALSE)
+
+            idx <- is.na(fData(object)$Flag) & fData(object)$Feature_ID %in% bad_qc
+            fData(object)$Flag[idx] <- "Low_qc_detection"
+
+            # Compute proportions found in each study group
+            if (!is.na(group)) {
+              proportions <- lcms_data(object)[, c("Sample_ID", group, featureNames(object))] %>%
+                tidyr::gather_("Feature_ID", "Intensity", featureNames(object)) %>%
+                dplyr::group_by_("Feature_ID", group) %>%
+                dplyr::summarise(proportion_found = prop_found(Intensity)) %>%
+                tidyr::spread_(group, "proportion_found")
+              # Remove a possible QC column
+              proportions$QC <- NULL
+              colnames(proportions)[-1] <- paste0("Detection_rate_", group, "_", colnames(proportions)[-1])
+              # Check if any group has enough non-missing entries
+              proportions$good <- apply(proportions[-1], 1, function(x){any(x >= group_limit)})
+
+              idx <- is.na(fData(object)$Flag) & (!fData(object)$Feature_ID %in% proportions$Feature_ID[proportions$good])
+              fData(object)$Flag[idx] <- "Low_group_detection"
+              # Add detection rates to feature data
+              proportions <- dplyr::left_join(proportions, found_qc_df, by = "Feature_ID")
+              proportions$good <- NULL
+            } else {
+              proportions <- found_qc_df
+            }
 
 
+
+            object <- join_fdata(object, proportions)
 
             object
-            })
+          })
 
