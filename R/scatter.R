f0aa334fcd84334fec053158d930b21108dd25af
Anton Mattsson <anton.matinpoika@gmail.com>
2019-02-27 16:39:59 +0200

    Density option added to scatter plots

--- R/scatter.R
+++ R/scatter.R
@@ -1,6 +1,6 @@
 
 
-plot_pca <- function(object, color = group_col(object), method = "ppca", center = TRUE, scale = "uv",
+plot_pca <- function(object, color = group_col(object), density = FALSE, method = "ppca", center = TRUE, scale = "uv",
                      title = "PCA", subtitle = NULL, color_scale = NULL, shape_scale = NULL, ...) {
 
   res_pca <- pcaMethods::pca(object, method = method, scale = scale, center = center, ...)
@@ -9,12 +9,12 @@ plot_pca <- function(object, color = group_col(object), method = "ppca", center 
   R2 <- res_pca@R2
   labels <- paste0(c("PC1", "PC2"), " (", scales::percent(R2), ")")
 
-  scatter_plot(pca_scores, x = "PC1", y = "PC2", xlab = labels[1], ylab = labels[2], color = color, shape = color, title = title,
-               subtitle = subtitle, color_scale = color_scale, shape_scale = shape_scale)
+  scatter_plot(pca_scores, x = "PC1", y = "PC2", xlab = labels[1], ylab = labels[2], color = color, shape = color,
+               density = density, title = title, subtitle = subtitle, color_scale = color_scale, shape_scale = shape_scale)
 }
 
 
-plot_tsne <- function(object, color = group_col(object), perplexity = 30, center = TRUE, scale = "uv",
+plot_tsne <- function(object, color = group_col(object), density = FALSE, perplexity = 30, center = TRUE, scale = "uv",
                       title = "t-SNE", subtitle = NULL, color_scale = NULL, shape_scale = NULL, ...) {
 
   prepd <- pcaMethods::prep(object, center = center, scale = scale)
@@ -23,12 +23,12 @@ plot_tsne <- function(object, color = group_col(object), perplexity = 30, center
   tsne_scores[color] <- pData(object)[, color]
 
 
-  scatter_plot(tsne_scores, x = "X1", y = "X2", color = color, shape = color, title = title,
-               subtitle = subtitle, color_scale = color_scale, shape_scale = shape_scale)
+  scatter_plot(tsne_scores, x = "X1", y = "X2", color = color, shape = color, density = density,
+               title = title, subtitle = subtitle, color_scale = color_scale, shape_scale = shape_scale)
 
 }
 
-scatter_plot <- function(data, x, y, color, shape, fixed = TRUE, color_scale = NULL, shape_scale = NULL,
+scatter_plot <- function(data, x, y, color, shape, density = FALSE, fixed = TRUE, color_scale = NULL, shape_scale = NULL,
                          title = NULL, subtitle = NULL, xlab = x, ylab = y,
                          color_lab = color, shape_lab = shape) {
 
@@ -66,6 +66,21 @@ scatter_plot <- function(data, x, y, color, shape, fixed = TRUE, color_scale = N
     p <- p +
       geom_point()
   }
+  # Add density plots to top and right
+  if (density) {
+    xdens <- cowplot::axis_canvas(p, axis = "x")+
+      geom_density(data = data, aes_string(x = x, fill = color),
+                   alpha = 0.7, size = 0.2)
 
+    ydens <- cowplot::axis_canvas(p, axis = "y", coord_flip = TRUE)+
+      geom_density(data = data, aes_string(x = y, fill = color),
+                   alpha = 0.7, size = 0.2)+
+      coord_flip()
+
+    p <- cowplot::insert_xaxis_grob(p, xdens, grid::unit(.2, "null"), position = "top")
+    p <- cowplot::insert_yaxis_grob(p, ydens, grid::unit(.2, "null"), position = "right")
+  }
+
+  p
   p
 }
