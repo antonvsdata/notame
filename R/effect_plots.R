460d67e3bcbbc6a7720f2257735b1c30f3b02b04
Anton Mattsson <anton.matinpoika@gmail.com>
2019-03-18 15:29:32 +0200

    Documentation for effect plots

--- R/effect_plots.R
+++ R/effect_plots.R
@@ -4,7 +4,7 @@
 #'
 #' Plots the change in the feature abundaces as a function of e.g. time.
 #' A line is drawn for each subject and a mean line is added.
-#' One plot is drawn for each feature
+#' A separate plot is drawn for each feature.
 #'
 #' @param object a MetaboSet object
 #' @param file path to the PDF file where the plots should be saved
@@ -20,7 +20,11 @@ save_subject_line_plots <- function(object, file, width = 8, height = 6,
 
   data <- combined_data(object)
 
-  for (fname in Biobase::featureNames(object)) {
+  for (i in seq_len(nrow(object))) {
+    if (i %% 500 == 0) {
+      cat(paste0("Iteration ", i, "/", nrow(object), "\n"))
+    }
+    fname <- Biobase::featureNames(object)[i]
 
     p <- ggplot(data, aes_string(x = x, y = fname)) +
       geom_line(aes_string(group = id), color = "grey20", alpha = 0.35, size = 0.3) +
@@ -41,7 +45,17 @@ save_subject_line_plots <- function(object, file, width = 8, height = 6,
   log_text(paste("Saved line plots with mean line to:", file))
 }
 
-
+#' Save box plots of each feature by group
+#'
+#' Draws a boxplot of feature abundances in each group.
+#' A separate plot is drawn for each feature.
+#'
+#' @param object a MetaboSet object
+#' @param file path to the PDF file where the plots should be saved
+#' @param width,height width and height of the plots in inches
+#' @param group character, name of the column to be used as x-axis and color
+#'
+#' @export
 save_group_boxplots <- function(object, file, width = 8, height = 6, group = group_col(object),
                                 color_scale =  NULL) {
 
@@ -51,7 +65,11 @@ save_group_boxplots <- function(object, file, width = 8, height = 6, group = gro
 
   data <- combined_data(object)
 
-  for (fname in Biobase::featureNames(object)) {
+  for (i in seq_len(nrow(object))) {
+    if (i %% 500 == 0) {
+      cat(paste0("Iteration ", i, "/", nrow(object), "\n"))
+    }
+    fname <- Biobase::featureNames(object)[i]
 
     p <- ggplot(data, aes_string(x = group, y = fname, color = group)) +
       geom_boxplot() +
@@ -67,3 +85,70 @@ save_group_boxplots <- function(object, file, width = 8, height = 6, group = gro
   log_text(paste("Saved group boxplots to:", file))
 
 }
+
+#' Save line plots with errorbars by group
+#'
+#' Plots the change in the feature abundaces as a function of e.g. time.
+#' A line is drawn for each group and errorbars are added.
+#' A separate plot is drawn for each feature.
+#'
+#' @param object a MetaboSet object
+#' @param file path to the PDF file where the plots should be saved
+#' @param width,height width and height of the plots in inches
+#' @param x character, name of the column to be used as x-axis
+#' @param group character, name of the column containing group information, used for coloring
+#' @param fun.data passed to ggplot2::stat_summary and used for errorbars,
+#' "A function that is given the complete data and should return a data frame with variables ymin, y, and ymax."
+#' @param fun.ymin,fun.y,fun.ymax Alternative to fun.data, passed to ggplot2::stat_summary,
+#' "supply three individual functions that are each passed a vector of x's and should return a single number"
+#' @param position_dodge_amout numeric: how much the group mean points should dodge away from each other
+#' @color_scale the color scale as returned by a ggplot function
+#'
+#' @export
+save_group_lineplots <- function(object, file, width = 8, height = 6,
+                                 x = time_col(object), group = group_col(object),
+                                 fun.data = "mean_cl_boot", fun.y = NULL,
+                                 fun.ymin = NULL, fun.ymax = NULL, position_dodge_amount = 0.2,
+                                 color_scale =  NULL) {
+
+  color_scale <- color_scale %||% getOption("amp.color_scale_dis")
+
+  pdf(file, width = width, height = height)
+
+  data <- combined_data(object)
+
+  for (i in seq_len(nrow(object))) {
+    if (i %% 500 == 0) {
+      cat(paste0("Iteration ", i, "/", nrow(object), "\n"))
+    }
+    fname <- Biobase::featureNames(object)[i]
+
+    p <- ggplot(data, aes_string(x = x, y = fname, group = group, color = group)) +
+      # Errorbars with solid lines
+      stat_summary(fun.data = fun.data,
+                   geom = "errorbar", width = 0.5,
+                   fun.y = fun.y, fun.ymin = fun.ymin, fun.ymax = fun.ymax,
+                   position = position_dodge(position_dodge_amount)) +
+      # Plot point to mean
+      stat_summary(fun.data = fun.data,
+                   geom = "point",
+                   fun.y = fun.y, fun.ymin = fun.ymin, fun.ymax = fun.ymax,
+                   position = position_dodge(position_dodge_amount), size = 4) +
+      # Line from mean to mean between for example timepoints
+      stat_summary(fun.data =fun.data,
+                   geom = "line",
+                   position = position_dodge(position_dodge_amount), size = 0.5,
+                   fun.y = fun.y, fun.ymin = fun.ymin, fun.ymax = fun.ymax) +
+      labs(title = fname, y = "Abundance") +
+      color_scale +
+      theme_bw()
+
+    plot(p)
+
+  }
+  dev.off()
+
+  log_text(paste("Saved line plots with mean line to:", file))
+}
+
+
