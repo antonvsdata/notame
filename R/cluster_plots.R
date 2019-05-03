ef537c2abcb151ff2750c298fda65a11841f1453
Anton Mattsson <anton.matinpoika@gmail.com>
2019-05-03 15:00:05 +0300

    Heatmaps and dendrograms of samples

--- R/cluster_plots.R
+++ R/cluster_plots.R
@@ -1,5 +1,23 @@
 
-
+#' Draw dendrograms
+#'
+#' Draws a dendrogram of a hierarchical clustering applied to the samples of an experiment
+#'
+#' @param object a MetaboSet object
+#' @param color character, name of the column used for coloring the sample labels
+#' @param dist_method distance method used in clustering, see ?dist
+#' @param clustering method used in clustering, see ?hclust
+#' @param center logical, should the data  be centered?
+#' @param scale scaling used, as in pcaMethods::prep. Default is "uv" for unit variance
+#' @param title The plot title
+#' @param subtitle The plot subtitle
+#' @param color_scale the color scale as returned by a ggplot function
+#'
+#' @return A ggplot object.
+#'
+#' @seealso \code{\link{dist}} \code{\link{hclust}}
+#'
+#' @export
 plot_dendrogram <- function(object, color = group_col(object), dist_method = "euclidean", clust_method = "ward.D2",
                      center = TRUE, scale = "uv", title = "Dendrogram of hierarchical clustering",
                      subtitle = NULL, color_scale = NULL) {
@@ -28,7 +46,8 @@ plot_dendrogram <- function(object, color = group_col(object), dist_method = "eu
 
   p <- ggplot(ggdendro::segment(d_data)) +
     geom_segment(aes(x=x, y=y, xend=xend, yend=yend)) +
-    geom_text(data = labels, aes_string(x="x", y="y", label = "label", color = color), angle = 90, hjust = 1) +
+    geom_text(data = labels, aes_string(x="x", y="y", label = "label", color = color),
+              angle = 90, hjust = 1) +
     ggdendro::theme_dendro() +
     color_scale +
     labs(title = title, subtitle = subtitle)
@@ -36,20 +55,73 @@ plot_dendrogram <- function(object, color = group_col(object), dist_method = "eu
   p
 
 }
-
-
-plot_heatmap <- function(object, text_color = group_col(object), dist_method = "euclidean", clust_method = "ward.D2",
-                         center = TRUE, scale = "uv", title = "Heatmap of distances between samples",
-                         subtitle = NULL, fill_scale = NULL, color_scale = NULL) {
-
+#' Draw heatmaps
+#'
+#' Draws a heatmap of the distances between the samples of an experiment,
+#' the samples are ordered by hierarchical clustering.
+#'
+#' @param object a MetaboSet object
+#' @param dist_method distance method used in clustering, see ?dist
+#' @param clustering method used in clustering, see ?hclust
+#' @param center logical, should the data  be centered?
+#' @param scale scaling used, as in pcaMethods::prep. Default is "uv" for unit variance
+#' @param group character, name of the column used for coloring the group bar
+#' @param title The plot title
+#' @param subtitle The plot subtitle
+#' @param fill_scale_con Continuous fill scale for the heatmap as returned by a ggplot function
+#' @param fill_scale_dis Discrete fill scale for the group bar as returned by a ggplot function
+#'
+#' @return if \code{group_bar} is \code{TRUE}, an object of class gTable (from the cowplot package).
+#' NOTE this object must be drawn with ggdraw() instead of plot(). Otherwise, a ggplot object.
+#'
+#' @seealso \code{\link{dist}} \code{\link{hclust}}
+#'
+#' @export
+plot_heatmap <- function(object, dist_method = "euclidean", clust_method = "ward.D2",
+                         center = TRUE, scale = "uv",
+                         group_bar = TRUE, group = group_col(object),
+                         title = "Heatmap of distances between samples",
+                         subtitle = NULL, fill_scale_con = NULL, fill_scale_dis = NULL) {
+  # Default settings
   subtitle <- subtitle %||% paste("Distance method:", dist_method, "Clustering method:", clust_method)
-  fill_scale <- fill_scale %||% getOption("amp.fill_scale_con")
-  color_scale <- color_scale %||% getOption("amp.color_scale_dis")
+  fill_scale_con <- fill_scale_con %||% getOption("amp.fill_scale_con")
+  fill_scale_dis <- fill_scale_dis %||% getOption("amp.fill_scale_dis")
 
+  # Distances
   distances <- dist(t(exprs(object)), method = dist_method)
-
+  # Hierarchical clustering for ordering
   hc <- hclust(distances, method = clust_method)
   hc_order <- hc$labels[hc$order]
 
+  # From wide to long format for ggplot
+  distances_df <- as.matrix(distances) %>%
+    as.data.frame() %>%
+    tibble::rownames_to_column("X") %>%
+    tidyr::gather("Y", "Distance", -X)
+  # Set the correct order given by hclust
+  distances_df$X <- factor(distances_df$X, levels = hc_order, ordered = TRUE)
+  distances_df$Y <- factor(distances_df$Y, levels = rev(hc_order), ordered = TRUE)
 
+  # Heatmap
+  p <- ggplot(distances_df, aes(X, Y, fill = Distance)) +
+    geom_tile(color = "white") +
+    fill_scale_con +
+    labs(x = NULL, y = NULL, title = title, subtitle = subtitle) +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = -0.05)) +
+    coord_fixed()
+  # Group bar
+  if (group_bar) {
+    pheno_data <- pData(object)
+    pheno_data$Sample_ID <- factor(pheno_data$Sample_ID, levels = hc_order, ordered = TRUE)
+
+    gb <- ggplot(pheno_data, aes_string(x = "Sample_ID", y = 1, fill = group)) +
+      geom_tile(color = "white") +
+      theme_void() +
+      fill_scale_dis
+
+    p <- cowplot::plot_grid(p, gb, ncol = 1, align = "v", rel_heights = c(10/11,1/11))
+  }
+
+  p
 }
