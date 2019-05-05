b20c8ff728039e1f9ffa06249face9515b79f6ee
Anton Mattsson <anton.matinpoika@gmail.com>
2019-05-05 11:42:15 +0300

    P-value histograms

--- R/qc_visualizations.R
+++ R/qc_visualizations.R
@@ -48,3 +48,65 @@ plot_dist_density <- function(object, dist_method = "euclidean", fill_scale = NU
                title = title, subtitle = subtitle)
 }
 
+#' Estimate the magnitude of drift
+#'
+#' Plots histograms of p-values from linear regression model, where each feature is predicted
+#' by injection order alone. The expected uniform distribution is represented by a dashed red line.
+#'
+#' @param object A MetaboSet object
+#'
+#' @return A ggplot object
+#'
+#' @seealso \code{\link{plot_p_histogram}}
+#'
+#' @export
+plot_injection_lm <- function(object) {
+
+  # Apply linear model to QC samples and biological samples separately
+  lm_all <- perform_lm(object, "Feature ~ Injection_order")
+  lm_sample <- perform_lm(object[, object$QC != "QC"], "Feature ~ Injection_order")
+  lm_qc <- perform_lm(object[, object$QC == "QC"], "Feature ~ Injection_order")
+
+  # Only interested in the p_values
+  p_values <- list("All samples" = lm_all$Injection_order_P,
+                   "Biological samples" = lm_sample$Injection_order_P,
+                   "QC samples" = lm_qc$Injection_order_P)
+  # Plotting
+  plot_p_histogram(p_values)
+}
+
+#' Histogram of p-values
+#'
+#' Draws histograms of p-values with expected uniform distribution represented by a dashed red line
+#'
+#' @param p_values list, each element is a vector of p-values. The list names are used as plot titles
+#'
+#' @return A ggplot object.
+#'
+#' @export
+plot_p_histogram <- function(p_values) {
+  # Custom breaks for the x-axis
+  breaks <- seq(0, 1, by = 0.05)
+
+  # THree separate histograms
+  plots <- list()
+  for (i in seq_along(p_values)) {
+    # Compute the position of the expected line
+    finite_count <- sum(is.finite(p_values[[i]]))
+    h_line <- finite_count/(length(breaks)-1)
+
+    p <- ggplot(data.frame(P = p_values[[i]]), aes(P)) +
+      geom_histogram(breaks = breaks, col = "grey50", fill = "grey80", size = 1) +
+      geom_hline(yintercept = h_line, color="red", linetype = "dashed", size = 1) +
+      labs(x="p-value", y="Frequency") +
+      ggtitle(names(p_values)[i]) +
+      theme_minimal() +
+      theme(plot.title = element_text(face="bold", hjust=0.5))
+
+    plots <- c(plots, list(p))
+  }
+  # Combine plots
+  p <- cowplot::plot_grid(plotlist = plots, ncol = 1)
+
+  p
+}
