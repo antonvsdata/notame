bff638fe1b118789a98f5b464b72f86927b9fa9f
Anton Mattsson <anton.matinpoika@gmail.com>
2019-07-09 15:33:49 +0300

    Added general introduction vignette

---
+++ inst/doc/intro.R
@@ -0,0 +1,9 @@
+## ----setup, include = FALSE----------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ---- out.heigth = "900px", echo=FALSE-----------------------------------
+knitr::include_graphics("amp_workflow.png")
+
