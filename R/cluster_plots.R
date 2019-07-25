529d946889cf5e5f0787cad7cd03d1598889a1e3
Anton Mattsson <anton.matinpoika@gmail.com>
2019-07-25 10:13:08 +0300

    Fixed typo in documentation

--- R/cluster_plots.R
+++ R/cluster_plots.R
@@ -65,11 +65,11 @@ plot_dendrogram <- function(object, all_features = FALSE, color = group_col(obje
 #'
 #' @param object a MetaboSet object
 #' @param all_features logical, should all features be used? If FALSE (the default), flagged features are removed before visualization.
-#' @param dist_method distance method used in clustering, see ?dist
-#' @param clust_method method used in clustering, see ?hclust
+#' @param dist_method distance method used in clustering, see \code{\link{dist}}
+#' @param clust_method clustering method used in clustering, see \code{\link{hclust}}
 #' @param center logical, should the data  be centered?
 #' @param scale scaling used, as in pcaMethods::prep. Default is "uv" for unit variance
-#' @param group_ber logical, should a bar showing the groups be drawn under the heat map?
+#' @param group_bar logical, should a bar showing the groups be drawn under the heat map?
 #' @param group character, name of the column used for coloring the group bar
 #' @param title The plot title
 #' @param subtitle The plot subtitle
