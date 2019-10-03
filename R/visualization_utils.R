1a220744c919867d185b00fc913c3c40ec856b7f
Anton Mattsson <antom@uef.fi>
2019-10-03 09:01:39 +0300

    Visualizations function with perplexity

--- R/visualization_utils.R
+++ R/visualization_utils.R
@@ -25,6 +25,7 @@ save_plot <- function(p, file, ...) {
 #'
 #' @param object A MetaboSet object
 #' @param prefix character, a file path prefix added to the file paths
+#' @param perplexity perplexity for t-SNE plots
 #' @param merge logical, whether the files should be merged to a single PDF, see Details
 #'
 #' @details If \code{merge} is \code{TRUE}, then a file containing all the visualizations
@@ -32,7 +33,7 @@ save_plot <- function(p, file, ...) {
 #' (\url{https://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/}) and on Linux you need to have pdfunite installed.
 #'
 #' @export
-visualizations <- function(object, prefix, all_features = FALSE, merge = FALSE) {
+visualizations <- function(object, prefix, perplexity = 30, merge = FALSE) {
 
   # Helper function
   file_names <- ""
@@ -41,13 +42,14 @@ visualizations <- function(object, prefix, all_features = FALSE, merge = FALSE) 
     save_plot(p, file = file_name, ...)
     file_names <<- paste(file_names, file_name)
   }
-  # Drop flagged features
-  object <- drop_flagged(object, all_features)
 
   if (sum(object$QC == "QC")) {
     save_name(plot_dist_density(object), "density_plot", width = 8, height = 6)
     save_name(plot_injection_lm(object), "lm_p_histograms")
   }
+  # Quality metrics
+  save_name(plot_quality(object), "quality_metrics")
+
   # Boxplots
   save_name(plot_sample_boxplots(object), "boxplots_group", width = 15)
   save_name(plot_sample_boxplots(object, order_by = "Injection_order",
@@ -61,11 +63,11 @@ visualizations <- function(object, prefix, all_features = FALSE, merge = FALSE) 
   save_name(plot_pca_hexbin(object), "PCA_hexbin")
   # t-SNE
   set.seed(38)
-  save_name(plot_tsne(object), "tSNE_group")
+  save_name(plot_tsne(object, perplexity = perplexity), "tSNE_group")
   set.seed(38)
-  save_name(plot_tsne(object, color = "Injection_order"), "tSNE_injection")
+  save_name(plot_tsne(object, perplexity = perplexity, color = "Injection_order"), "tSNE_injection")
   set.seed(38)
-  save_name(plot_tsne_hexbin(object), "tSNE_hexbin")
+  save_name(plot_tsne_hexbin(object, perplexity = perplexity), "tSNE_hexbin")
   # Clustering
   save_name(plot_dendrogram(object), "dendrogram", width = 15)
   save_name(plot_sample_heatmap(object), "heatmap_samples", width = 15, height = 16)
