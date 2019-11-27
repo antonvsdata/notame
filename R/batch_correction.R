5c080a8f58e4aadf7171f3e7e8095ff9e2e1a7ba
Anton Mattsson <antom@uef.fi>
2019-11-27 12:05:40 +0200

    Combined batch effect functions to one file

--- R/batch_correction.R
+++ R/batch_correction.R
@@ -65,3 +65,240 @@ ruvs_qc <- function(object, batch, replicates, k = 3, ...) {
   pData(object) <- cbind(pData(object), ruv_results$W)
   object
 }
+
+
+#' Bhattacharyya distance between bathces in PCA space
+#'
+#' Computes Bhattacharyya distance between all pairs of batches after
+#' projecting the samples into PCA space with pcaMethods::pca
+#'
+#' @param object a MetaboSet object
+#' @param batch column name of pData givinh the batch labels
+#'
+#' @return matrix of Bhattacharyya distances between batches
+#'
+#' @export
+pca_bhattacharyya_dist <- function(object, batch, all_features = FALSE, center = TRUE, scale = "uv", nPcs = 3, ...) {
+  # Drop flagged features if not told otherwise
+  object <- drop_flagged(object, all_features)
+
+  # PCA to 2 dimenstions
+  pca_res <- pcaMethods::pca(object, center = center, scale = scale, nPcs = nPcs, ...)
+  pca_scores <- pcaMethods::scores(pca_res)
+
+  # Split to batches
+  batches <- list()
+  for (b in unique(pData(object)[, batch])) {
+    batches[[b]] <- pca_scores[pData(object)[, batch] == b, ]
+  }
+
+  # Compute means and covariance matrices for Bhattacharyya distance
+  muarray <- sapply(batches, colMeans)
+  sigmaarray <- array(sapply(batches, cov), dim = c(nPcs, nPcs, length(batches)))
+
+  fpc::bhattacharyya.matrix(muarray,sigmaarray,ipairs="all", misclassification.bound = FALSE)
+}
+
+
+pooled_variance <- function(x, group) {
+  # Remove missing values
+  group <- group[!is.na(x)]
+  x <- x[!is.na(x)]
+  # Split to groups
+  group_list <- split(x, group)
+  n_1 <- sapply(group_list, length) - 1 # n - 1
+  # Pooled variance
+  sum(n_1 * sapply(group_list, var)) / sum(n_1)
+}
+
+between_variance <- function(x, group) {
+  # Remove missing values
+  group <- group[!is.na(x)]
+  x <- x[!is.na(x)]
+  # Split to groups
+  group_list <- split(x, group)
+  n <- sapply(group_list, length)
+  means <- sapply(group_list, mean)
+  k_1 <- length(unique(group)) - 1
+  # Between group variance formula
+  sum(n * (means - mean(x))^2) / k_1
+}
+
+repeatability <- function(x, group) {
+  pv <- pooled_variance(x, group)
+  bv <- between_variance(x, group)
+  bv / (bv + pv)
+}
+
+
+#' Compute repeatability measures
+#'
+#' Computes repeatability for each feature with the following formula:
+#' \deqn{\frac{\sigma^2_{between}}{\sigma^2_{between} + \sigma^2_{within}}}
+#' The repeatability ranges from 0 to 1.
+#'
+#'
+#' @param object a MetaboSet object
+#' @param group column name of pData givinh the group labels
+#'
+#' @return data frame with one row per feature with the repeatability measure
+#'
+#' @export
+perform_repeatability <- function(object, group) {
+
+  group <- pData(object)[, group]
+  repeatabilities <- foreach::foreach(feature = featureNames(object), .combine = rbind) %dopar% {
+    result_row <- data.frame(Feature_ID = feature,
+                             Repeatability = repeatability(exprs(object)[feature, ], group))
+  }
+  repeatabilities
+}
+
+#' Align features between batches
+#'
+#' Aligns features with m/z or retention time shift between batches using alignBatches from batchCorr package.
+#' See more details in the help file and the original paper.
+#'
+#' @param object_na a MetaboSet object with missing values as NA
+#' @param object_fill a similar MetaboSet object with imputed values
+#' (used to compute distances between features, can contain missing values as well)
+#' @param batch character, column name of pData with batch labels
+#' @param mz,rt column names of m/z and retention time columns in fData
+#' @param mzdiff,rtdiff the windows for m/z and retention time for aligning features
+#' @param plot_folder path to the location where the plots should be saved, if NULL, no plots are saved
+#'
+#' @return a MetaboSet object with the aligned features
+align_batches <- function(object_na, object_fill, batch, mz, rt, mzdiff, rtdiff, plot_folder = NULL) {
+
+  # Set working directory for plotting (the bathCorr functions saves plots in the current working directory...)
+  if (!is.null(plot_folder)) {
+    old_wd <- getwd()
+    setwd(plot_folder)
+    report <- TRUE
+  } else {
+    report <- FALSE
+  }
+
+
+  # Extract peak mz and rt information
+  pInfo <- as.matrix(fData(object_na)[, c(mz, rt)])
+  colnames(pInfo) <- c("mz", "rt")
+
+  # Align batches based on the QCs
+  aligned <- batchCorr::alignBatches(peakInfo = pInfo, PeakTabNoFill = t(exprs(object_na)), PeakTabFilled = t(exprs(object_fill)),
+                                     batches = pData(object_na)[, batch], sampleGroups = object_na$QC, selectGroup = "QC",
+                                     mzdiff = mzdiff, rtdiff = rtdiff, report = report)
+
+  # Reset working directory
+  if (!is.null(plot_folder)) {
+    setwd(old_wd)
+  }
+
+  # Attach aligned features
+  exprs(object_fill) <- t(aligned$PTalign)
+  object_fill
+}
+
+
+#' Normalize batches
+#'
+#' Normalize bathces by either reference samples of population median.
+#' Uses normalizeBatches function from the batchCorr package
+#'
+#' @param object a MetaboSet object
+#' @param batch,group character, column names of pData with batch labels and group labels
+#' @param ref_label the label of the reference group i.e. the group that is constant through batches
+#' @param ... additional parameters passed to batchCorr::normalizeBatches
+#'
+#' @return list, the object with normalized features and information on which features were corrected by ref samples in each batch.
+normalize_batches <- function(object, batch, group = group_col(object), ref_label, ...) {
+
+  normData <- batchCorr::normalizeBatches(peakTable = t(exprs(object)), batches = pData(object)[, batch],
+                                          sampleGroup = pData(object)[, group], refGroup = ref_label, ...)
+
+  exprs(object) <- t(normData$peakTable)
+  ref_corrected <- as.data.frame(t(normData$refCorrected))
+  colnames(ref_corrected) <- paste0("Ref_corrected_", seq_len(ncol(ref_corrected)))
+  ref_corrected$Feature_ID <- featureNames(object)
+  object <- join_results(object, ref_corrected)
+}
+
+#' Save batch correction plots
+#'
+#' Saves plots of each feature showing the effect of batch correction.
+#' Plots show QC samples and regular samples inside each batch, plus the
+#' batch mean for all samples and QC samples as a horizontal line.
+#' NOTE: if you change the shape variable, be sure to set a shape scale as well,
+#' the default scale only has 2 values, so it can only accomodate 2 shapes.
+#'
+#' @param orig,corrected MetaboSet objects before and after batch effect correction
+#' @param file path to the PDF file where the plots will be saved
+#' @param batch,color,shape column names of pData for batch labels,
+#' and column used for coloring and shaping points (by default batch and QC)
+#' @param color_scale,shape_scale scales for color and scale as returned by ggplot functions.
+save_batch_plots <- function(orig, corrected, file, batch = "Batch", color = "Batch", shape = "QC",
+                             color_scale = NULL, shape_scale = NULL) {
+
+  color_scale <- color_scale %||% getOption("amp.color_scale_dis")
+  shape_scale <- shape_scale %||% scale_shape_manual(values = c(15, 21))
+
+  data_orig <- combined_data(orig)
+  data_corr <- combined_data(corrected)
+
+  batch_injections <- data_orig %>%
+    dplyr::group_by(!! sym(batch)) %>%
+    dplyr::summarise(start = min(Injection_order), end = max(Injection_order))
+
+  batch_mean_helper <- function(data) {
+    data %>%
+      dplyr::group_by(!! sym(batch)) %>%
+      dplyr::summarise_at(featureNames(orig), finite_mean) %>%
+      dplyr::left_join(batch_injections, ., by = batch)
+  }
+
+  get_batch_means <- function(data) {
+    batch_means <- batch_mean_helper(data) %>%
+      dplyr::mutate(QC = FALSE)
+    batch_means_qc <- data %>%
+      dplyr::filter(QC == "QC") %>%
+      batch_mean_helper() %>%
+      dplyr::mutate(QC = TRUE)
+
+    rbind(batch_means, batch_means_qc)
+  }
+
+  batch_means_orig <- get_batch_means(data_orig)
+
+
+  batch_means_corr <- get_batch_means(data_corr)
+
+
+  batch_plot_helper <- function(data, fname, batch_means) {
+    p <- ggplot() +
+      geom_point(data = data, mapping = aes_string(x = "Injection_order", y = fname,
+                                                   color = color, shape = shape)) +
+      theme_bw() +
+      theme(panel.grid = element_blank()) +
+      color_scale +
+      shape_scale
+
+    p <- p +
+      geom_segment(data = batch_means, mapping = aes_string(x = "start", xend = "end",
+                                                            y = fname, yend = fname,
+                                                            color = color, linetype = "QC"))
+    p
+  }
+
+  pdf(file)
+
+  for (feature in featureNames(orig)) {
+    p1 <- batch_plot_helper(data_orig, feature, batch_means_orig)
+
+    p2 <- batch_plot_helper(data_corr, feature, batch_means_corr)
+
+    p <- cowplot::plot_grid(p1, p2, nrow = 2)
+    plot(p)
+  }
+
+  dev.off()
+}
