d696506b66b529c95220c2464f9ce92e4e0f3f6b
Anton Mattsson <anton.matinpoika@gmail.com>
2020-03-25 08:52:49 +0200

    Removed inst/doc

--- inst/doc/MetaboSet.R
+++
@@ -1,9 +0,0 @@
-## ----setup, include = FALSE---------------------------------------------------
-knitr::opts_chunk$set(
-  collapse = TRUE,
-  comment = "#>"
-)
-
-## ---- out.width = "600px", echo=FALSE-----------------------------------------
-knitr::include_graphics("Data_input.png")
-
--- inst/doc/intro.R
+++
@@ -1,9 +0,0 @@
-## ----setup, include = FALSE---------------------------------------------------
-knitr::opts_chunk$set(
-  collapse = TRUE,
-  comment = "#>"
-)
-
-## ---- out.heigth = "900px", echo=FALSE----------------------------------------
-knitr::include_graphics("notame_workflow.png")
-
