6801ebe4889e00299cbc0d015851d79c2702018b
Anton Mattsson <anton.matinpoika@gmail.com>
2022-01-19 18:42:59 +0200

    check_position no longer gives warning about type.convert.default

--- R/class_constructor.R
+++ R/class_constructor.R
@@ -81,7 +81,7 @@ check_pheno_data <- function(x, id_prefix, log_messages = FALSE) {
 # Check that the position of the corner row and column is OK
 check_position <- function(x, cc, cr) {
   condition <- is.na(x[cr - 1, cc - 1]) &
-    (is.numeric(type.convert(x[cr + 1, cc + 1])) | is.na(x[cr + 1, cc + 1])) &
+    (is.numeric(type.convert(x[cr + 1, cc + 1], as.is = TRUE)) | is.na(x[cr + 1, cc + 1])) &
     !is.na(x[cr, cc]) &
     !is.na(x[cr - 1, cc]) &
     !is.na(x[cr, cc - 1])
