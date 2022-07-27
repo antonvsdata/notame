c1388f402572154569ed0f3da6a342ed82996438
attlih <atte.lihtamo@gmail.com>
2022-07-27 12:12:45 +0300

    Fix test_logging
    expect_null didn't work

--- tests/testthat/test_logging.R
+++ tests/testthat/test_logging.R
@@ -3,12 +3,12 @@ context("Testing global options")
 library(notame)
 
 test_that("Error handling changes when logging and is set back to default after logging", {
-  expect_null(options()$error)
+  expect_equal(options()$error, NULL)
   log_file <- tempfile()
   init_log(log_file)
   expect_equal(class(options()$error), "call")
   finish_log()
-  expect_null(options()$error)
+  expect_equal(options()$error, NULL)
 })
 
 
