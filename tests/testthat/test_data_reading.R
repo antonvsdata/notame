9652b827c432b2a74ac93e59422b5b1c63ce83e6
attlih <atte.lihtamo@gmail.com>
2022-07-27 12:50:21 +0300

    Fix dummy injection error
    dummy_injection was not found during merging if x was already merged

--- tests/testthat/test_data_reading.R
+++ tests/testthat/test_data_reading.R
@@ -186,13 +186,24 @@ test_that("Creating dummy injection order works as expected", {
   sampleNames(modes$HILIC_neg)[2] <- "ID_666"
   sampleNames(modes$RP_pos)[22] <- "ID_999"
 
-  merged <- merge_metabosets(modes)
-
+  expect_warning(merged <- merge_metabosets(modes),
+                 regexp = "Sample IDs are not identical|Unequal amount of samples"
+  )
+  # Dummy injection
   expect_equal(merged$Injection_order, -seq_along(merged$Sample_ID))
-  expect_equal(sort(as.numeric(na.omit(merged$HILIC_neg_Injection_order))), modes$HILIC_neg$Injection_order)
-  expect_equal(sort(as.numeric(na.omit(merged$HILIC_pos_Injection_order))), modes$HILIC_pos$Injection_order)
-  expect_equal(sort(as.numeric(na.omit(merged$RP_neg_Injection_order))), modes$RP_neg$Injection_order)
-  expect_equal(sort(as.numeric(na.omit(merged$RP_pos_Injection_order))), modes$RP_pos$Injection_order)
+  # Original IOs
+  expect_equal(sort(as.numeric(na.omit(merged$HILIC_neg_Injection_order))),
+               modes$HILIC_neg$Injection_order
+  )
+  expect_equal(sort(as.numeric(na.omit(merged$HILIC_pos_Injection_order))),
+               modes$HILIC_pos$Injection_order
+  )
+  expect_equal(sort(as.numeric(na.omit(merged$RP_neg_Injection_order))),
+               modes$RP_neg$Injection_order
+  )
+  expect_equal(sort(as.numeric(na.omit(merged$RP_pos_Injection_order))),
+               modes$RP_pos$Injection_order
+  )
 })
 
 
