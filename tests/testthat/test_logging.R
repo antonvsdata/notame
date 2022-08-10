6ea261d4dbba1d950422de1e86e45b3753783c79
attlih <atte.lihtamo@gmail.com>
2022-08-10 12:03:24 +0300

    Add tests for logging

--- tests/testthat/test_logging.R
+++ tests/testthat/test_logging.R
@@ -1,15 +1,41 @@
-context("Testing global options")
+context("Testing logging")
 
 library(notame)
 
-test_that("Error handling changes when logging and is set back to default after logging", {
-  expect_equal(options()$error, NULL)
+test_that("Error/warning handling changes when logging and is set to NULL after logging", {
+  # Save current error handling
+  orig_error <- options("error")
+  orig_warn <- options("warning.expression")
   log_file <- tempfile()
   init_log(log_file)
-  expect_equal(class(options()$error), "call")
+  # Initiating log should change error handling
+  expect_failure(expect_identical(options("error")$error, orig_error$error))
   finish_log()
-  expect_equal(options()$error, NULL)
+  # Finishing log should set error to "NULL"
+  expect_null(options("error")$error)
+  expect_null(options("warning.expression")$warning.expression)
 })
 
+test_that("Logging works as expected", {
+  log_file <- tempfile()
+  log_text("New line")
+  # File doesn't exist yet
+  expect_warning(expect_error(readLines(log_file), "cannot open the connection"))
 
+  init_log(log_file)
+  logs <- readLines(log_file)
+  # Last line should match
+  expect_identical(gsub("\\[.+\\] " ,"" , logs[length(logs)]), "INFO Starting logging")
+  log_text("New line")
+  logs <- readLines(log_file)
+  # Last line should match
+  expect_identical(gsub("\\[.+\\] " ,"" , logs[length(logs)]), "INFO New line")
+  finish_log()
+  logs <- readLines(log_file)
+  log_len <- length(logs)
+  # This shouldn't log to file anymore
+  log_text("New line")
+  logs <- readLines(log_file)
+  expect_identical(log_len, length(logs))
+})
 
