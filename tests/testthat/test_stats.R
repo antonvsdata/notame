4427000ff224ff94ee7acaa4750f1d24f99404af
attlih <98050742+attlih@users.noreply.github.com>
2022-08-26 13:38:14 +0300

    Enabled pairwise paired t-tests (#17)
    * Enabled pairwise paired t-tests
    
    * Changed column names
    
    Changed column names to match new uniform style

--- tests/testthat/test_stats.R
+++ tests/testthat/test_stats.R
@@ -1,7 +1,7 @@
 context("Testing statistics")
 
 library(notame)
-
+# Summary statistics ----
 test_that("summary statistics work without grouping", {
   # Force foreach to count sequentially
   foreach::registerDoSEQ()
@@ -17,9 +17,9 @@ test_that("summary statistics work without grouping", {
 
 
 test_that("summary statistics work with grouping", {
+  foreach::registerDoSEQ()
 
   smry <- summary_statistics(mark_nas(example_set, 0), grouping_cols = "Group")
-
   exa <- exprs(mark_nas(example_set[, example_set$Group == "A"], 0))
 
   for (fun in c("finite_mean", "finite_sd", "finite_median", "finite_mad")) {
@@ -49,7 +49,7 @@ test_that("summary statistics work with all NA features", {
   expect_true(all(is.na(smry[1, 2:ncol(smry)])))
 })
 
-
+# Cohen's D ----
 test_that("Cohen's d works", {
   ex <- example_set %>%
     drop_qcs() %>%
@@ -92,6 +92,20 @@ test_that("Cohen's d work with all NA features", {
   expect_true(all(is.na(cohd[1:2, 2:ncol(cohd)])))
 })
 
+test_that("Cohen's d is not run with multiple time or group levels", {
+
+  expect_error(cohens_d(example_set), "Column Group")
+
+  ex <- example_set
+  ex$Group <- c(1, 2)
+  expect_error(cohens_d(ex, id = "Subject_ID", time = "Time"), "Column Time")
+
+  ex$Time <- c(1,2)
+  ex$Group <- 1
+  expect_error(cohens_d(ex, id = "Subject_ID", time = "Time"), "Column Group")
+})
+
+# Fold change ----
 test_that("Fold change works", {
   ex <- example_set %>%
     mark_nas(0)
@@ -135,7 +149,7 @@ test_that("Fold change works with all NA features", {
   expect_true(all(is.na(foldc[1:2, 2:ncol(foldc)])))
 })
 
-
+# P-value correction ----
 test_that("P-value correction works", {
 
   ps <- data.frame(x = letters,
@@ -157,7 +171,7 @@ test_that("P-value correction works", {
 
 })
 
-
+# Linear model ----
 test_that("Linear model works", {
 
   cd <- combined_data(drop_qcs(example_set))
@@ -194,6 +208,7 @@ test_that("Linear model works", {
 
 })
 
+# Logistic regression ----
 test_that("Logistic regression works", {
 
   cd <- combined_data(drop_qcs(example_set))
@@ -375,5 +390,91 @@ test_that("Cohens D warnings work", {
   )
 })
 
+
+# Paired t-test ----
+test_that("Paired t-test works", {
+  ex <- drop_qcs(example_set)
+  cd <- combined_data(ex)
+  t_res <- perform_paired_t_test(ex, group = "Time", id = "Subject_ID")
+  feature <- "HILIC_pos_259_9623a4_4322"
+  mean1 <- finite_mean(cd[cd$Time == 1, colnames(cd) == feature])
+  mean2 <- finite_mean(cd[cd$Time == 2, colnames(cd) == feature])
+  # Check comparison order
+  expect_equal(t_res[t_res$Feature_ID == feature, 2], mean1 - mean2)
+  # Check row names
+  expect_identical(rownames(t_res), featureNames(drop_qcs(example_set)))
+  # Check column names
+  expect_identical(colnames(t_res), c(
+    "Feature_ID",
+    "1_vs_2_Estimate",
+    "1_vs_2_Lower_CI95",
+    "1_vs_2_Upper_CI95",
+    "1_vs_2_t_test_P",
+    "1_vs_2_t_test_P_FDR"
+  ))
+})
+
+# Pairwise t-tests ----
+test_that("Pairwise t-test works", {
+  object <- drop_qcs(example_set)
+  pData(object)$Group <- factor(rep(c(rep("A", 3), rep("B", 3), rep("C", 2)), 3))
+  pData(object)$Subject_ID <- factor(rep(1:8, 3))
+  pData(object)$Time <- factor(c(rep(1, 8), rep(2, 8), rep(3, 8)))
+
+  pwt_res <- perform_pairwise_t_test(object, group = "Time")
+
+  expect_identical(rownames(pwt_res), featureNames(drop_qcs(example_set)))
+  prefixes <- c("1_vs_2_", "1_vs_3_", "2_vs_3_")
+  suffixes <- c("Estimate", "Lower_CI95", "Upper_CI95", "t_test_P", "t_test_P_FDR")
+  cols <- expand.grid(prefixes, suffixes)
+  # Check column names
+  expect_identical(colnames(pwt_res), c(
+    "Feature_ID", "1_Mean", "2_Mean",
+    do.call(paste0, cols[order(cols$Var1) & cols$Var1 == prefixes[1], ]),
+    "3_Mean",
+    do.call(paste0, cols[order(cols$Var1), ])[6:15]
+  ))
+  # These should be identical as no paired mode
+  pData(object)$Subject_ID <- factor(rep(1:12, 2))
+  expect_identical(perform_pairwise_t_test(object, group = "Time"), pwt_res)
+  # These shouldn't match cause paired mode
+  # In this case 4 pairs in each
+  expect_failure(expect_identical(perform_pairwise_t_test(object,
+                                                          group = "Time",
+                                                          id = "Subject_ID",
+                                                          is_paired = TRUE
+  ), pwt_res))
+})
+
+test_that("Pairwise paired t-test works", {
+  object <- drop_qcs(example_set)
+  pData(object)$Group <- factor(rep(c(rep("A", 3), rep("B", 3), rep("C", 2)), 3))
+  pData(object)$Subject_ID <- factor(rep(1:8, 3))
+  pData(object)$Time <- factor(c(rep(1, 8), rep(2, 8), rep(3, 8)))
+
+  pwpt_res <- perform_pairwise_t_test(object,
+                                      group = "Time",
+                                      id = "Subject_ID",
+                                      is_paired = TRUE
+  )
+
+  expect_identical(rownames(pwpt_res), featureNames(drop_qcs(example_set)))
+  prefixes <- c("1_vs_2_", "1_vs_3_", "2_vs_3_")
+  suffixes <- c("Estimate", "Lower_CI95", "Upper_CI95", "t_test_P", "t_test_P_FDR")
+  cols <- expand.grid(prefixes, suffixes)
+  expect_identical(colnames(pwpt_res), c("Feature_ID",
+                                        do.call(paste0, cols[order(cols$Var1), ])
+  ))
+  # Change Subject IDs
+  pData(object)$Subject_ID <- factor(rep(1:12, 2))
+  pwpt_res_2 <- perform_pairwise_t_test(object,
+                                        group = "Time",
+                                        id = "Subject_ID",
+                                        is_paired = TRUE
+  )
+  # These shouldn't match because means are counted only from paired samples
+  # In this case 4 pairs in each
+  expect_failure(expect_identical(pwpt_res_2, pwpt_res))
+})
 
 
