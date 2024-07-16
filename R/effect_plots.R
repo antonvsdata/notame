06a2f5b9b00aee43df0f2a464ae7ea36b4f2764b
attlih <atte.lihtamo@afekta.com>
2024-07-16 13:27:12 +0300

    Fix deprecated syntax

--- R/effect_plots.R
+++ R/effect_plots.R
@@ -150,24 +150,24 @@ save_subject_line_plots <- function(object,
         geom_line(aes(group = .data[[id]]),
           color = "grey20",
           alpha = 0.35,
-          size = line_width
+          linewidth = line_width
         ) +
         stat_summary(aes(group = 1),
           fun.data = "mean_se",
           geom = "line",
-          size = mean_line_width,
+          linewidth = mean_line_width,
           color = color_scale$palette(1)[1]
         )
     } else {
       p <- p +
         geom_line(aes(group = .data[[id]], color = .data[[color]]),
           alpha = 0.35,
-          size = line_width
+          linewidth = line_width
         ) +
         stat_summary(aes(group = .data[[color]], color = .data[[color]]),
           fun.data = "mean_se",
           geom = "line",
-          size = mean_line_width
+          linewidth = mean_line_width
         ) +
         color_scale
     }
